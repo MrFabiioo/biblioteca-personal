7822cd024371e72124694d764fa916cba0c1626a
MrFabiioo <fabiolin57@gmail.com>
2024-09-27 10:28:15 -0500

    add useRoutes

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -1,20 +1,27 @@
 //import { useState } from 'react'
+import {useRoutes,BrowserRouter} from 'react-router-dom'
 import Home from '../Home';
 import Books from '../Books'
 import SingIn from '../SingIn'
 import NotFound from '../NotFound'
 import './App.css'
 
+const AppRoutes = ()=>{
+  let routes = useRoutes([
+    {path: '/', element: <Home/>},
+    {path: '/books',element:<Books/>},
+    {path: '/sing-in',element:<SingIn/>},
+    {path: '/*',element:<NotFound/>}
+  ])
+
+  return routes;
+}
+
 function App() {
-
-
   return (
-    <div className='bg-red-100'>
-      <Home/>
-      <Books/>
-      <SingIn/>
-      <NotFound/>
-    </div>
+    <BrowserRouter>
+      <AppRoutes/>
+    </BrowserRouter>
 
   )
 }
