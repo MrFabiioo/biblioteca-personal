79071bbb4181cc48f5f7ca9ac13f2c4acfa842f7
Fabio Ortega <fabiolin57@gmail.com>
2025-02-20 11:29:57 -0500

    añadidos logos, iconos y favicon

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -1,29 +1,33 @@
+import { SlSocialInstagram } from "react-icons/sl";
+import { FaGithub } from "react-icons/fa";
+import { FaHome } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
 function Footer(){
     return(
 <footer className="bg-white  shadow-sm dark:bg-gray-900 mt-auto">
     <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div className="sm:flex sm:items-center sm:justify-between">
-            <a href="https://flowbite.com/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-                <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
+            <a href="https://flowbite.com/" className="flex items-center mb-8 sm:mb-0 space-x-3 rtl:space-x-reverse">
+                <img src="/src/images/logo.png" className="h-8 rounded-xl" alt="Flowbite Logo" />
                 <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Biblioteca Personal</span>
             </a>
-            <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+            <ul className="flex flex-wrap items-center aling-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
                 <li>
-                    <a href="#" className="hover:underline me-4 md:me-6">About</a>
+                    <a href="/" className="hover:underline me-4 md:me-6 flex"><FaHome /></a>
                 </li>
                 <li>
-                    <a href="#" className="hover:underline me-4 md:me-6">Privacy Policy</a>
+                    <a href="#" className="hover:underline me-4 md:me-6 flex"><SlSocialInstagram /></a>
                 </li>
                 <li>
-                    <a href="#" className="hover:underline me-4 md:me-6">Licensing</a>
+                    <a href="#" className="hover:underline me-4 md:me-6 flex"><FaGithub /></a>
                 </li>
                 <li>
-                    <a href="#" className="hover:underline">Contact</a>
+                    <a href="#" className="hover:underline me-4 md:me-6 flex"><FaFacebook /></a>
                 </li>
             </ul>
         </div>
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-        <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" className="hover:underline">Flowbite™</a>. All Rights Reserved.</span>
+        <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2025 <a href="https://flowbite.com/" className="hover:underline">Jose Fabio Ortega Estrada™</a>. Todos los derechos reservados.</span>
     </div>
 </footer>
     );
