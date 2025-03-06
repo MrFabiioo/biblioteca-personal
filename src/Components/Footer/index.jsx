f15953a2c76ea651deb26a7340325555103f3776
Fabio Ortega <fabiolin57@gmail.com>
2025-03-06 10:35:29 -0500

    agregando fuente mono a lo necesario

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -30,7 +30,7 @@ function Footer(){
             </ul>
         </div>
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-        <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2025 <a href="https://flowbite.com/" className="hover:underline">Jose Fabio Ortega Estrada™</a>. Todos los derechos reservados.</span>
+        <span className="font-mono block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2025 <a href="https://flowbite.com/" className="hover:underline">Jose Fabio Ortega Estrada™</a>. Todos los derechos reservados.</span>
     </div>
 </footer>
     );
--- src/Components/NavBar/index.jsx
+++ src/Components/NavBar/index.jsx
@@ -51,21 +51,21 @@ function NavBar (){
       
         <div className="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow-sm dark:bg-gray-700 dark:divide-gray-600" id="user-dropdown">
         <div className="px-4 py-3">
-          <span className="block text-sm text-gray-900 dark:text-white">{user.name}</span>
-          <span className="block text-sm  text-gray-500 truncate dark:text-gray-400">{user.email}</span>
+          <span className="font-mono block text-sm text-gray-900 dark:text-white">{user.name}</span>
+          <span className="font-mono block text-sm  text-gray-500 truncate dark:text-gray-400">{user.email}</span>
         </div>
         <ul className="py-2" aria-labelledby="user-menu-button">
           <li>
-            <a href="#" onClick={()=>loginWithRedirect()} className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Log In</a>
+            <a href="#" onClick={()=>loginWithRedirect()} className="font-mono block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Log In</a>
           </li>
           <li>
-            <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Opciones</a>
+            <a href="#" className="font-mono block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Opciones</a>
           </li>
           <li>
-            <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Temas</a>
+            <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white font-mono">Temas</a>
           </li>
           <li>
-            <a href="#" onClick={()=>logout()} className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Salir</a>
+            <a href="#" onClick={()=>logout()} className="font-mono block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Salir</a>
           </li>
         </ul>
       </div>
@@ -81,7 +81,7 @@ function NavBar (){
   <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-user">
     <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
       <li>
-            <NavLink  onClick={()=>context.setSearchByCategory()} to='/' className ="block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0">
+            <NavLink  onClick={()=>context.setSearchByCategory()} to='/' className ="font-mono block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0">
                     Todo
             </NavLink>
       </li>
