fe66898c345d7c820f980ddef02edf0028ff42df
Fabio Ortega <fabiolin57@gmail.com>
2025-03-11 17:16:54 -0500

    cambiando ruta de logo

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -11,7 +11,7 @@ function Footer(){
     <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div className="sm:flex sm:items-center sm:justify-between">
             <a href="#" className="flex items-center mb-8 sm:mb-0 space-x-3 rtl:space-x-reverse">
-                <img src="/src/images/Creo.png" className="h-8 rounded-xl" alt="Flowbite Logo" />
+                <img src="/images/Creo.png" className="h-8 rounded-xl" alt="Flowbite Logo" />
                 <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Biblioteca Personal</span>
             </a>
             <ul className="flex flex-wrap items-center aling-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
