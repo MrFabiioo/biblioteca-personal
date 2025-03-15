051901980432d89049760d55f7f58bd57f0d7c2d
Fabio Ortega <fabiolin57@gmail.com>
2025-03-15 11:17:14 -0500

    arreglando menu de picture

--- src/Components/BookDetails/index.jsx
+++ src/Components/BookDetails/index.jsx
@@ -30,11 +30,8 @@ function BookDetail() {
             },
           });
         }
-
         const response = await getOneBook(id, token);
-
         setBook(response);
-
       } catch (error) {
 
         if (error.response.data?.message === "Invalid Compact JWS" || "Unauthorized") {
@@ -76,188 +73,86 @@ function BookDetail() {
 
   return (
     <>
-
-
-
       <Layout>
 
         <div className="w-full px-7">
-
-
           <div className="w-full h-36  rounded-xl my-3 " style={{ backgroundImage: `url(${book?.review?.bannerImage})` }}></div>
-
         </div>
-
-
-
-
-
         <div className="relative isolate overflow-hidden bg-white px-6 mb-24 lg:overflow-visible lg:px-0">
-
           <div className="absolute inset-0 -z-10 overflow-hidden">
-
             <svg
-
               aria-hidden="true"
-
               className="absolute top-0 left-[max(50%,25rem)] h-[64rem] w-[128rem] -translate-x-1/2 stroke-gray-200 [mask-image:radial-gradient(64rem_64rem_at_top,white,transparent)]"
-
             >
-
               <defs>
-
                 <pattern
-
                   x="50%"
-
                   y={-1}
-
                   id="e813992c-7d03-4cc4-a2bd-151760b470a0"
-
                   width={200}
-
                   height={200}
-
                   patternUnits="userSpaceOnUse"
-
                 >
-
                   <path d="M100 200V.5M.5 .5H200" fill="none" />
-
                 </pattern>
-
               </defs>
-
               <svg x="50%" y={-1} className="overflow-visible fill-gray-50">
-
                 <path
-
                   d="M-100.5 0h201v201h-201Z M699.5 0h201v201h-201Z M499.5 400h201v201h-201Z M-300.5 600h201v201h-201Z"
-
                   strokeWidth={0}
-
                 />
-
               </svg>
-
               <rect fill="url(#e813992c-7d03-4cc4-a2bd-151760b470a0)" width="100%" height="100%" strokeWidth={0} />
-
             </svg>
-
           </div>
-
           <div className="mx-auto mt-2 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-start lg:gap-y-10">
-
             <div id="1" className="lg:col-span-1 lg:col-start-1 lg:row-start-1 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-1 lg:gap-x-8 lg:px-8">
-
-              <div className="lg:pr-4">
-
+              <div className="lg:pr-4 ">
                 <div className="">
-
                   <p className="text-base/7 font-semibold text-blue-700 tracking-widest">{book.title}</p>
-
                   <h1 className="mt-3 text-4xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-5xl">
-
                     {book.review?.title}
-
                   </h1>
-
                   <h2 className="mt-6 text-2xl font-bold tracking-tight text-gray-900 tracking-widest">Sobre el autor: </h2>
-
                   <img className="m-3 w-20 h-20 rounded-full object-cover " src={book?.review?.authorImage} alt="user photo" />
-
-                  <p className="text-base/7 font-semibold text-blue-700 tracking-widest">{book.author}</p>
-
+                 <p className="text-base/7 font-semibold text-blue-700 tracking-widest">{book.author}</p>
                   <p className="mt-3 text-base/7 text-gray-700 tracking-widest text-justify ">
-
                     {book.review?.aboutAuthor}
-
                   </p>
-
                 </div>
-
               </div>
-
             </div>
-
-
             <div className="-mt-12 -ml-12 p-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden hidden lg:block">
-
               <img
-
                 alt=""
-
                 src={book?.review?.imageOne}
-
                 className=" w-[21rem] max-w-none rounded-xl bg-gray-900 ring-1 shadow-xl shadow-neutral-500  ring-gray-400/10 sm:w-[57rem]"
-
               />
-
               <img
-
                 alt=""
-
                 src={book?.review?.imageTwo}
-
                 className="mt-12  w-[21rem] max-w-none rounded-xl bg-gray-900 ring-1 shadow-xl shadow-neutral-500 ring-gray-400/10 sm:w-[57rem]"
-
-
               />
-
               <img
-
                 alt=""
-
                 src={book?.review?.imageThree}
-
                 className="mt-12 w-[21rem] max-w-none rounded-xl bg-gray-900 ring-1 shadow-xl shadow-neutral-500 ring-gray-400/10 sm:w-[57rem]"
-
-
               />
-
             </div>
-
             <div className="lg:col-span-1 lg:col-start-1 lg:row-start-2 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-1 lg:gap-x-8 lg:px-8">
-
               <div className="lg:pr-4">
-
                 <div className=" text-base/7 text-gray-700 ">
-
                   <h2 className="mt-6 text-2xl font-bold tracking-tight text-gray-900 tracking-widest">Introducción.</h2>
-
                   <p id="2" className="mt-3 tracking-widest text-justify">
-
                     {book.review?.introduction}
-
                   </p>
-
-
-
-
-
                   <div className="lg:hidden grid place-items-center ">
-
-
                     <img
-
-
                       alt=""
-
-
                       src={book?.review?.imageOne}
-
-
                       className="mt-12  w-[21rem] bg-gray-900 ring-1 shadow-xl shadow-neutral-500 ring-gray-400/10 sm:w-[57rem]"
-
-
                     />
-
-
                   </div>
-
-
-
-
                   {/* <ul role="list" className="mt-8 space-y-8 text-gray-600">
 
                 <li className="flex gap-x-3">
@@ -305,89 +200,37 @@ function BookDetail() {
                 </li>
 
               </ul> */}
-
                   <h2 className="mt-6 text-2xl font-bold tracking-tight text-gray-900">Revisión.</h2>
-
                   <p id="3" className="mt-3 tracking-widest text-justify">
-
                     {book.review?.review}
-
                   </p>
-
-
-
-
-
                   <div className="lg:hidden grid place-items-center">
-
-
                     <img
-
-
                       alt=""
-
-
                       src={book?.review?.imageTwo}
-
-
                       className="mt-12 w-[21rem] bg-gray-900 ring-1 shadow-xl shadow-neutral-500 ring-gray-400/10 sm:w-[57rem]"
-
-
                     />
-
-
                   </div>
-
-
-
-
                   <h2 className="mt-6 text-2xl font-bold tracking-tight text-gray-900">Conclusion.</h2>
-
                   <p className="mt-3 tracking-widest text-justify">
-
                     {book.review?.conclusion}
-
                   </p>
-
-
                   <div className="mt-8 lg:hidden grid place-items-center">
-
-
                     <img
-
-
                       alt=""
-
-
                       src={book?.review?.imageThree}
-
-
                       className=" w-[21rem]   bg-gray-900 ring-1 shadow-xl shadow-neutral-500  ring-gray-400/10 sm:w-[57rem]"
-
-
                     />
-
-
                   </div>
-
                   <h2 className="mt-6 text-2xl font-bold tracking-tight text-gray-900">Critica.</h2>
-
-                  <p className="mt-3 tracking-widest text-justify">
-
+                <p className="mt-3 tracking-widest text-justify">
                     {book.review?.criticism}
-
                   </p>
-
                 </div>
-
               </div>
-
             </div>
-
           </div>
-
         </div>
-
       </Layout>
 
     </>
--- src/Components/NavBar/index.jsx
+++ src/Components/NavBar/index.jsx
@@ -1,14 +1,17 @@
 import { NavLink } from "react-router-dom";
 import {RequestApiBooks} from '../../Context';
-import { useContext,useState,useEffect } from "react";
+import { useContext,useState,useEffect,useRef } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import {getAllCategories} from "../../services/category.service"
-import 'flowbite'
+//import 'flowbite'
 
 function NavBar (){
     const {user,loginWithRedirect,logout,isAuthenticated,getAccessTokenSilently}= useAuth0();
     const context = useContext(RequestApiBooks)
     const [categories,setCategories] = useState([]);
+    const [open,setOpen]= useState(false);
+    const menuRef = useRef(null);
+    const buttonRef = useRef(null);
     
 
     useEffect(() => {
@@ -33,10 +36,31 @@ function NavBar (){
     
       getCategories();
     }, [isAuthenticated, getAccessTokenSilently]); 
+
+    useEffect(() => {
+      function handleClickOutside(event) {
+        // Verificar si el clic fue fuera del menú Y del botón
+        if (
+          menuRef.current &&
+          !menuRef.current.contains(event.target) &&
+          buttonRef.current &&
+          !buttonRef.current.contains(event.target)
+        ) {
+          setTimeout(() => setOpen(false), 100); // Evita el conflicto del estado
+        }
+      }
+  
+      document.addEventListener("mousedown", handleClickOutside);
+      return () => {
+        document.removeEventListener("mousedown", handleClickOutside);
+      };
+    }, []);
     
+
     
     return (
       isAuthenticated &&
+      <>
 <nav className="bg-blue-200 border-gray-200 dark:bg-gray-900 ">
   <div className=" flex flex-wrap items-center justify-between mx-auto p-4">
   <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
@@ -44,33 +68,10 @@ function NavBar (){
       <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Biblioteca Personal</span>
   </a>
   <div className="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-      <button  type="button" className="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
+      <button ref={buttonRef} onClick={(e) => {e.stopPropagation(); setOpen((prev) => !prev);}} type="button" className="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
         <span className="sr-only">Open user menu</span>
         <img  className="w-8 h-8 rounded-full" src={user.picture} alt="user photo"/>
       </button>
-      
-        <div className="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow-sm dark:bg-gray-700 dark:divide-gray-600" id="user-dropdown">
-        <div className="px-4 py-3">
-          <span className="font-mono block text-sm text-gray-900 dark:text-white">{user.name}</span>
-          <span className="font-mono block text-sm  text-gray-500 truncate dark:text-gray-400">{user.email}</span>
-        </div>
-        <ul className="py-2" aria-labelledby="user-menu-button">
-          <li>
-            <a href="#" onClick={()=>loginWithRedirect()} className="font-mono block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Log In</a>
-          </li>
-          <li>
-            <a href="#" className="font-mono block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Opciones</a>
-          </li>
-          <li>
-            <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white font-mono">Temas</a>
-          </li>
-          <li>
-            <a href="#" onClick={()=>logout()} className="font-mono block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Salir</a>
-          </li>
-        </ul>
-      </div>
-      
-      
       <button data-collapse-toggle="navbar-user" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-user" aria-expanded="false">
         <span className="sr-only">Open main menu</span>
         <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
@@ -78,6 +79,7 @@ function NavBar (){
         </svg>
     </button>
   </div>
+  
   <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-user">
     <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-blue-200 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
       <li>
@@ -97,7 +99,32 @@ function NavBar (){
     </ul>
   </div>
   </div>
-</nav>
+</nav>  
+      { open && 
+        <div ref={menuRef} className="w-48 z-50 absolute right-0 top-12  mt-2 my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow-sm dark:bg-gray-700 dark:divide-gray-600" id="user-dropdown">
+        <div className="px-4 py-3">
+          <span className="font-mono block text-sm text-gray-900 dark:text-white">{user.name}</span>
+          <span className="font-mono block text-sm  text-gray-500 truncate dark:text-gray-400">{user.email}</span>
+        </div>
+        <ul className="py-2" aria-labelledby="user-menu-button">
+          <li>
+            <a href="#" onClick={()=>loginWithRedirect()} className="font-mono block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Log In</a>
+          </li>
+          <li>
+            <a href="#" className="font-mono block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Opciones</a>
+          </li>
+          <li>
+            <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white font-mono">Temas</a>
+          </li>
+          <li>
+            <a href="#" onClick={()=>logout()} className="font-mono block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Salir</a>
+          </li>
+        </ul>
+      </div>
+      }
+        
+
+      </>
     );
 }
 export default NavBar
